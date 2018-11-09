48cba22d681d6222f68e05c6cbaefa83e5c2c05a
Carl <rdelsol35@gmail.com>
2018-11-10 00:01:37 +0800

    Update home page

--- src/components/catalogItem.jsx
+++ src/components/catalogItem.jsx
@@ -8,13 +8,15 @@ export class CatalogItem extends React.Component {
     render() {
         return (
             <td>
-                <div className='ml-3 mr-3'>
-                    <img className={"card-img-top image-350-200 card-item-image mb-3"} src={this.props.image} alt={"Card image cap"} />
-                    <div className={'d-flex justify-content-center align-items-center flex-column h-100'}>
-                        <h4 className={'font-weight-light mb-1 text-center'}>{this.props.name}</h4>
-                        <p className={'text-extra-small text-muted'}>{this.props.description}</p>
+                <a href={'#'} className={'text-plain'}>
+                    <div className='ml-3 mr-3'>
+                        <img className={"card-img-top image-350-270 card-item-image mb-3"} src={this.props.image} alt={"Card image cap"} />
+                        <div className={'d-flex justify-content-center align-items-center flex-column h-100'}>
+                            <h4 className={'font-weight-light mb-1 text-center'}>{this.props.name}</h4>
+                            <p className={'text-extra-small text-muted'}>{this.props.description}</p>
+                        </div>
                     </div>
-                </div>
+                </a>
             </td>
     )
     }
